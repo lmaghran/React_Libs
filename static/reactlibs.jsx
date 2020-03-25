a49d8cf0d40f21764d81a24f93e79db2a83ced5b
Lauren Maghran <maghranlauren@gmail.com>
2020-03-25 00:04:25 +0000

    added first component

---
+++ static/reactlibs.jsx
@@ -0,0 +1,18 @@
+
+function MyApp() {
+    return (
+    <div>
+    <ul>
+    <li>1</li>
+    <li>2</li>
+    <li>3</li>
+    </ul>
+    </div>
+
+    )
+}
+
+ReactDOM.render(
+  <MyApp />,
+  document.getElementById('root')
+);
