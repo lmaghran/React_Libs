8638b9ec05119059450d4f1d01f243270823167b
Lauren Maghran <maghranlauren@gmail.com>
2020-03-27 20:40:40 +0000

    adding array data into react appp

--- static/reactlibs.jsx
+++ static/reactlibs.jsx
@@ -1,91 +1,71 @@
+const productsData = [
+    {
+        id: "1",
+        name: "Pencil",
+        price: 1,
+        description: "Perfect for those who can't remember things! 5/5 Highly recommend."
+    },
+    {
+        id: "2",
+        name: "Housing",
+        price: 0,
+        description: "Housing provided for out-of-state students or those who can't commute"
+    },
+    {
+        id: "3",
+        name: "Computer Rental",
+        price: 300,
+        description: "Don't have a computer? No problem!"
+    },
+    {
+        id: "4",
+        name: "Coffee",
+        price: 2,
+        description: "Wake up!"
+    },
+    {
+        id: "5",
+        name: "Snacks",
+        price: 0,
+        description: "Free snacks!"
+    },
+    {
+        id: "6",
+        name: "Rubber Duckies",
+        price: 3.50,
+        description: "To help you solve your hardest coding problems."
+    },
+    {
+        id: "7",
+        name: "Fidget Spinner",
+        price: 21.99,
+        description: "Because we like to pretend we're in high school."
+    },
+    {
+        id: "8",
+        name: "Sticker Set",
+        price: 14.99,
+        description: "To prove to other devs you know a lot."
+    }
+]
 
-
-
-
-
-
-function ContactCard(props) {
+function Product(props) {
     return (
-        <div className="contact-card">
-                <img src={props.contact.imgUrl}/>
-                <h3>{props.contact.name}</h3>
-                <p>Phone: {props.contact.phone}</p>
-                <p>Email: {props.contact.email}</p>
-            </div>
+        <div>
+            <h2>{props.product.name}</h2>
+            <p>{props.product.price.toLocaleString("en-US", { style: "currency", currency: "USD" })} - {props.product.description}</p>
+        </div>
     );
 }
 
-
-function MainContent () {
-    return(
-    <div className="contacts">
-                        <ContactCard 
-                contact={{name: "Mr. Whiskerson", imgUrl: "http://placekitten.com/300/200", phone: "(212) 555-1234", email: "mr.whiskaz@catnap.meow"}}
-            />
-            
-            <ContactCard 
-                contact={{name: "Fluffykins", imgUrl: "http://placekitten.com/400/200", phone: "(212) 555-2345", email: "fluff@me.com"}}
-            />
-            
-            <ContactCard
-                contact={{name: "Destroyer", imgUrl: "http://placekitten.com/400/300", phone: "(212) 555-3456", email: "ofworlds@yahoo.com"}}
-            />
-            
-            <ContactCard 
-                contact={{name: "Felix", imgUrl: "http://placekitten.com/200/100", phone: "(212) 555-4567", email: "thecat@hotmail.com"}}
-            />
-
-    </div>
-    );
-}
-
-function Header () {
-    const date = new Date()
-  const hours = date.getHours()
-  let timeOfDay
-  const styles = {
-    color: "#FF8C00", 
-    backgroundColor: "#FF2D00",
-    fontSize: 24
-  }
-  
-  if (hours < 12) {
-    timeOfDay = "morning"
-    styles.color = "#04756F"
-  } else if (hours >= 12 && hours < 17) {
-    timeOfDay = "afternoon"
-    styles.color = "#2E0927"
-  } else {
-    timeOfDay = "night"
-    styles.color = "#D90000"
-  }
-
-    return(
-    <div>
-    <header className="navbar"> To Do List </header>
-    <h1 style= {styles}> It is currently about {date.getHours()} o'clock!,  Good {timeOfDay} !</h1>
-    </div>
-    )
-}
-
-
-
-function Footer () {
-    return(
-    <footer>
-        <h3> This is my footer </h3>
-    </footer>
-    )
-}
 
 
 function MyApp() {
+    const productComponents= productsData.map(item => <Product product={item}/>);
     return (
 
-    <div className="todo-list">
-    <Header />
-    <MainContent />
-    <Footer />
+    <div>
+       {productComponents}
     </div>
 
     )
